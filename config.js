38510ea55c3f155c836bb95497cae5629e3dc13b
Tudumodzz <arunodsankalpa648@gmail.com>
2024-08-17 18:06:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://images.app.goo.gl/1kWa6Eo1aRi3b26DA",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hellow, I am SANKALPA MD.i am Alive now !",
 };
