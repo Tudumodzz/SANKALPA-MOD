2d173e5ba98a9bbc94d5c85582d014a93656268f
Tudumodzz <arunodsankalpa648@gmail.com>
2024-08-18 18:33:38 +0530

    Update Songs&video.js

--- plugins/Songs&video.js
+++ plugins/Songs&video.js
@@ -1,1 +1,48 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
 
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please giye me url or title")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url 
+
+let desc = `
+💫 SANKALPA-MD YT SONG DOWNLOADER 💫
+
+title: ${data.title}
+description: ${data.description}
+time: ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
+
+POWERD BY SANKALPA-MD 💚
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{qouted:mek});
+  
+//download audio
+
+let down = await fg.yta(url)
+let downloadUrl = down.dl_url
+
+//send audio message
+await conn.sendMessage(from,{audio:{url:downloadUrl},mimetype:"audio/mpeg"},{qouted:mek})
+
+  
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+  
+
+  
